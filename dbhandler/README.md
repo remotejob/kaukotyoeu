f101ef1b1379dac53bb854003fb06dddc373acf3
juno <juno@krasava.fi>
2016-09-23 05:06:07 +0300

    dev

--- dbhandler/README.md
+++ dbhandler/README.md
@@ -2,17 +2,9 @@
 --
     import "github.com/remotejob/kaukotyoeu/dbhandler"
 
-Package dbhandler different DB functions
-
+Package dbhandler ----> different DB functions
 
 ### List
-
-In the files of a library package, you can attach a comment to the package name,
-which will be shown in the overview for that package's documentation. Just like
-for command documentation, the convention is to use a file called doc.go for
-your package documentation, though it is not necessary. If you have comments on
-the package name in multiple files, they'll be concatenated, but generally it's
-best to just have comments in one file.
 
 ## Usage
 
@@ -21,7 +13,7 @@ best to just have comments in one file.
 ```go
 func GetAllForStatic(session mgo.Session, site string) []domains.Articlefull
 ```
-GetAllForStatic coments
+GetAllForStatic get database records for static pages
 
 #### func  GetAllSitemaplinks
 
--- dbhandler/dbhandler.go
+++ dbhandler/dbhandler.go
@@ -1,13 +1,7 @@
-// Package dbhandler different DB functions
+// Package dbhandler ----> different DB functions
 //
 // List
 //
-// In the files of a library package, you can attach a comment to the package
-// name, which will be shown in the overview for that package's documentation.
-// Just like for command documentation, the convention is to use a file called
-// doc.go for your package documentation, though it is not necessary.  If you
-// have comments on the package name in multiple files, they'll be concatenated,
-// but generally it's best to just have comments in one file.
 package dbhandler
 
 import (
@@ -34,7 +28,7 @@ func InsertLogRecord(session mgo.Session, record domains.LogRecord) {
 
 }
 
-//GetAllForStatic coments
+//GetAllForStatic get database records for static pages
 func GetAllForStatic(session mgo.Session, site string) []domains.Articlefull {
 
 	session.SetMode(mgo.Monotonic, true)
